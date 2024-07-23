a71bc58fe584372dde43617c06414dc09501018c
KISHORE7812883161 <kishore4919215@gmail.com>
2024-07-23 11:48:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/saveetha-web/', // This is the base URL of your GitHub Pages site
+ // This is the base URL of your GitHub Pages site
 
 })
